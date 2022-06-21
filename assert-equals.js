1c42c1050fa7259ec9604421db79ab7ced0f8972
lisahns <lisa.hanes@hotmail.de>
2022-06-21 17:43:47 +0100

    finished take-home challenge

--- assert-equals.js
+++ assert-equals.js
@@ -1,3 +1,26 @@
-function assertEquals(expect, actual) {}
+function assertEquals(expect, actual) {
+    if (expect == actual) return "Success";
+
+    if (typeof expect !== typeof actual) {
+        throw "Expected '" + typeof(expect) + "' but found '" + typeof(actual) + "'"
+    }
+
+    if (Array.isArray(expect) && Array.isArray(actual) && expect.length !== actual.length) {
+        throw "Expected array length " + expect.length + " but found " + actual.length
+    }
+
+    if (Array.isArray(expect) && Array.isArray(actual) && expect.length === actual.length 
+    && expect.every((value, index) => value === actual[index])) {
+        return "Success"
+    }
+
+    if (Array.isArray(expect) && Array.isArray(actual) && expect.length === actual.length && expect.filter((value, index) => value !== actual[index])) {
+        const compareArr = expect.filter((value, index) => value !== actual[index])
+        const actualVal = actual.filter((value, index) => value !== expect[index])
+        throw "Expected '" + compareArr + "' but found '" + actualVal + "'"
+    }
+
+    else throw "Expected '" + expect + "' but found '" + actual + "'"; 
+}
 
 module.exports = assertEquals
--- assert-equals.spec.js
+++ assert-equals.spec.js
@@ -1,13 +1,55 @@
+const { TestScheduler } = require('jest')
 const assertEquals = require('./assert-equals')
 
-describe('assertEquals', () => {
-  describe('when expected and actual are the same string', () => {
-    it('returns without throwing an error', () => {
-      expect(() => assertEquals('abc', 'abc')).not.toThrow()
-    })
-  })
+// describe('assertEquals', () => {
+//   describe('when expected and actual are the same string', () => {
+//     it('returns without throwing an error', () => {
+//       expect(() => assertEquals('abc', 'abc')).not.toThrow()
+//     })
+//   })
 
-  describe('when expected and actual are different strings', () => {
-    it.todo('throws an error')
-  })
+//   describe('when expected and actual are different strings', () => {
+//     it.todo('throws an error')
+//   })
+// })
+
+test("compares two same strings", () => {
+  const test = assertEquals("abc", "abc");
+  expect(test)
+})
+test("compares to values that are not the same", () => {
+  const test = assertEquals("abdef", "abc");
+  expect(test)
+})
+test("compare type of", () => {
+  const test = assertEquals("abc", 1);
+  expect(test)
+})
+test("compares two same numbers", () => {
+  const test = assertEquals(1, 1);
+  expect(test)
+})
+test("compares array to different primitive", () => {
+  const test = assertEquals(['a', 'b', 'c'], "abc");
+  expect(test)
+})
+test("compares array length", () => {
+  const test = assertEquals(['a', 'b', 'c'], ['a', 'b', 'c', 'd']);
+  expect(test)
+})
+test("compares arrays", () => {
+  const test = assertEquals(['a', 'b', 'c'], ['a', 'b', 'c']);
+  expect(test)
+})
+test("compares arrays with partial different values", () => {
+  const test = assertEquals(['a', 'b', 'c'], ['a', 'b', 'd']);
+  expect(test)
+})
+test("boolean and number", () => {
+  const test = assertEquals(true, 2);
+  expect(test)
+})
+test("true and false", () => {
+  const test = assertEquals(true, false);
+  expect(test)
 })
